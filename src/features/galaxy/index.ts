6f88cdd19992611c21a8eef98599b664a671cd08
BeeBoo175 <xiaoqinhew@gmail.com>
2026-01-16 19:45:18 +0800

    feat(spaceship): implement interactive landing spaceship prototype
    - introduce 3D spaceship mesh rendered in the planetary system
    - implement trajectory interpolation toward focused planet coordinates

--- src/features/galaxy/index.ts
+++ src/features/galaxy/index.ts
@@ -1,4 +1,6 @@
 export { default as GalaxyScene } from "./GalaxyScene";
 export type { GalaxySceneProps } from "./GalaxyScene";
+export { default as Spaceship } from "./Spaceship";
 export * from "./types";
 export * from "./data";
+
